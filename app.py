94224af3971adcb853c9ee71660087c12faa3acb
Matthew Shirley <matthewshirley321@gmail.com>
2026-09-20 15:58:26 -0500

    task 3: online

--- app.py
+++ app.py
@@ -24,3 +24,11 @@ def get_devices():
 @app.get("/devices/hottest")
 def get_hottest():
     return hottest(readings)
+
+@app.get("/devices/online")
+def get_online():
+    online_devices = []
+    for device in readings:
+        if device["online"]:
+            online_devices.append(device)
+    return online_devices
